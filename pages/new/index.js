1ac7e4b3449d44478f12628f36e284295a0b1593
Long Tran <nhulongctk10@gmail.com>
2019-01-13 21:57:54 +0700

    Deploy script using now

--- pages/new/index.js
+++ pages/new/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { withRouter } from 'next/router';
 
-const NewDetails = ({ data, ...rest }) => {
+const NewDetails = ({ data = {} }) => {
   return (
-    console.log(rest) || (
-      <div className="new-item new-item--bottom">
-        <a className="new-item__thumb" href={data.urlToImage} target="_blank">
-          <img src={data.urlToImage} alt="New thumb" />
-        </a>
-        <div className="new-body">
-          <h2 className="new-item__title u-collor-primary">
-            <a target="_blank" href={data.url}>
-              {data.title}
-            </a>
-          </h2>
-          <p className="new-item__content">{data.description}</p>
-        </div>
+    <div className="new-item new-item--bottom">
+      <a className="new-item__thumb" href={data.urlToImage} target="_blank">
+        <img src={data.urlToImage} alt="New thumb" />
+      </a>
+      <div className="new-body">
+        <h2 className="new-item__title u-collor-primary">
+          <a target="_blank" href={data.url}>
+            {data.title}
+          </a>
+        </h2>
+        <p className="new-item__content">{data.description}</p>
       </div>
-    )
+    </div>
   );
 };
 
