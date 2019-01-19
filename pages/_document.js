5246efe63a5d27d14bd12261f47bea31467e5924
Long Tran <nhulongctk10@gmail.com>
2019-01-19 15:30:09 +0700

    Fix PWA

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,10 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>News App Using Next.js</title>
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
